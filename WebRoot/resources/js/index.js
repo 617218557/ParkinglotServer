008f16f6f24fea188b7ff6c441e7330ac0eb364a
fengyifei <617218557@qq.com>
2016-02-24 10:23:14 +0800

    完善登录界面，添加后台管理界面

--- WebRoot/resources/js/index.js
+++ WebRoot/resources/js/index.js
@@ -3,8 +3,14 @@ $(document).ready(function () {
     $("#btn-login").on("click", function () {
         $.get("adminlogincl", $('#login-info').serialize(),
             function (data) {
-                alert($.base64.decode(data));
-                return data;
+                var result = $.parseJSON($.base64.decode(data));
+                if (result.code == 1) {
+                    // 跳转管理界面
+                    self.location='manage.html'; 
+                } else {
+                    // 显示错误信息
+                    alert(result.msg);
+                }
             });
     });
 });
